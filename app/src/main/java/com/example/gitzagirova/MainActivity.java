bd19e5f3b334bc5986916875509304efba8a39e8
Iamlyuba <113868864+Iamlyuba@users.noreply.github.com>
2023-01-17 15:51:19 +0900

    Initial commit

---
+++ app/src/main/java/com/example/gitzagirova/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.gitzagirova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        GitHubService gitHubService = GitHubService.retrofit.create(GitHubService.class);
+        final Call<List<Contributor>> call =
+                gitHubService.repoContributors("square", "picasso");
+
+        call.enqueue(new Callback<List<Contributor>>() {
+            @Override
+            public void onResponse(Call<List<Contributor>> call, Response<List<Contributor>> response) {
+                final TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText(response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Call<List<Contributor>> call, Throwable throwable) {
+                final TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("Что-то пошло не так: " + throwable.getMessage());
+            }
+        });
+    }
+}
+class Contributor {
+
+    private String login;
+    private int contributions;
+
+    // И другие поля
+    //String html_url;
+
+    @Override
+    public String toString() {
+        return login + " (" + contributions + ")";
+    }
+}
+interface GitHubService {
+    // GET /repos/:owner/:repo/contributors
+
+    @GET("repos/{owner}/{repo}/contributors")
+    Call<List<Contributor>> repoContributors(
+            @Path("owner") String owner,
+            @Path("repo") String repo);
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
